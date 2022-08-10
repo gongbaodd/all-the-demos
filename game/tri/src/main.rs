24d388200a486694f4f7af4397f7d12f7f25b9ca
gongbaodd@InspironWSL <gongbaodd@qq.com>
2022-08-10 15:09:27 +0800

    drawing

--- game/tri/src/main.rs
+++ game/tri/src/main.rs
@@ -43,9 +43,10 @@ fn main() {
     let mut event_pump = sdl.event_pump().unwrap();
 
     let vertices: Vec<f32> = vec![
-        -0.5, -0.5, 0.0, //
-        0.5, -0.5, 0.0, //
-        0.0, 0.5, 0.0,
+        // positions        // colors
+        0.5, -0.5, 0.0, /* */ 1.0, 0.0, 0.0, //
+        -0.5, -0.5, 0.0, /* */ 0.0, 1.0, 0.0, //
+        0.0, 0.5, 0.0, /* */ 0.0, 0.0, 1.0,
     ];
 
     let mut vbo: gl::types::GLuint = 0;
@@ -75,8 +76,18 @@ fn main() {
             3,
             gl::FLOAT,
             gl::FALSE,
-            (3 * std::mem::size_of::<f32>()) as gl::types::GLint,
+            (6 * std::mem::size_of::<f32>()) as gl::types::GLint,
             std::ptr::null(),
+        );
+
+        gl::EnableVertexAttribArray(1);
+        gl::VertexAttribPointer(
+            1,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            (6 * std::mem::size_of::<f32>()) as gl::types::GLint,
+            (3 * std::mem::size_of::<f32>()) as *const gl::types::GLvoid,
         );
 
         gl::BindBuffer(gl::ARRAY_BUFFER, 0);
