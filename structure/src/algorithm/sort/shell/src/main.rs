da26c455898f28416d7580af80ba30fc80bc8449
gongbaodd@InspironWSL <gongbaodd@qq.com>
2022-08-15 14:13:32 +0800

    shell sort

--- structure/src/algorithm/sort/shell/src/main.rs
+++ structure/src/algorithm/sort/shell/src/main.rs
@@ -12,23 +12,25 @@ fn shell_sort(data: &mut Vec<i32>) {
 
     while gap > 0 {
         for i in gap..len {
-            let temp = data[i];
             let mut j = i;
-
-            while j >= gap && data[j - gap] > temp {
+            while j >= gap && data[j] < data[j - gap] {
                 data.swap(j, j - gap);
                 j -= gap;
             }
-
-            data[j] = temp;
         }
-
         gap /= 2;
     }
 }
 
 #[cfg(test)]
 mod test {
+    use super::*;
+
     #[test]
-    pub fn test() {}
+    pub fn test() {
+        let mut data = vec![33, 32, 39, 30, 22, 11, 44];
+        shell_sort(&mut data);
+
+        assert_eq!(data, vec![11, 22, 30, 32, 33, 39, 44]);
+    }
 }
