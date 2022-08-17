85a98aa2d4f2536e4ff779c94f3d46f46171aecc
gongbaodd@Inspiron_PS <gongbaodd@qq.com>
2022-08-17 15:15:51 +0800

    add comments

--- structure/src/algorithm/sort/heap/src/main.rs
+++ structure/src/algorithm/sort/heap/src/main.rs
@@ -6,16 +6,19 @@ fn main() {
 
 fn heap(data: &mut Vec<i32>) {
     if data.len() < 2 {
-        return;
+        return ;
     }
 
-    let last_root = (data.len()+2)/2;
+    // build one max heap
+    let last_root = (data.len() + 2) / 2;
     for root in (0..=last_root).rev() {
         to_max_heap(data, root, data.len() - 1);
     }
 
+    // replace the first element(max element) to the last
+    // make max heap using the other elements
     for last in (1..data.len()).rev() {
-        data.swap(last, 0);
+        data.swap(0, last);
         to_max_heap(data, 0, last-1);
     }
 }
@@ -43,7 +46,7 @@ fn to_max_heap(data: &mut Vec<i32>, mut root: usize, last: usize) {
         if data[max_child] > data[root] {
             data.swap(max_child, root);
         }
-        root = max_child;
 
+        root = max_child;
     }
 }
