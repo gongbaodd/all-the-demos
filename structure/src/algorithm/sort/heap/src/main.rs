d8cfce39774a5adcf3140d16ed10cdd1652e3004
gongbaodd@InspironWSL <gongbaodd@qq.com>
2022-08-17 10:38:29 +0800

    heap

--- structure/src/algorithm/sort/heap/src/main.rs
+++ structure/src/algorithm/sort/heap/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     let mut data = vec![33, 32, 39, 30, 22, 11, 44];
+    heap(&mut data);
     println!("{:?}", data);
 }
 
@@ -7,13 +8,20 @@ fn heap<T: Ord>(data: &mut [T]) {
     if data.len() <= 1 {
         return;
     }
+
+    heapify(data);
+
+    for end in (1..data.len()).rev() {
+        data.swap(0, end);
+        move_down(&mut data[..end], 0);
+    }
 }
 
 fn heapify<T: Ord>(data: &mut [T]) {
     /*
-            33
-        32      39
-    30     22  11    44
+                [0]33
+        [1]32          [2]39
+    [3]30   [4]22  [5]11    [6]44
     */
 
     let last_parent = (data.len() - 2) / 2;
@@ -23,4 +31,24 @@ fn heapify<T: Ord>(data: &mut [T]) {
     }
 }
 
-fn move_down<T: Ord>(data: &mut [T], mut root: usize) {}
+fn move_down<T: Ord>(data: &mut [T], mut root: usize) {
+    let last = data.len() - 1;
+
+    loop {
+        let left = 2 * root + 1;
+        if left > last {
+            break;
+        }
+        let right = left + 1;
+        let max = if right < last && data[right] > data[left] {
+            right
+        } else {
+            left
+        };
+
+        if data[max] > data[root] {
+            data.swap(root, max);
+        }
+        root = max;
+    }
+}
