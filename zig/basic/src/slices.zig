5a250bbfd94ac0f4afe5290316379d02c95540ad
gongbaodd@InspironWSL <gongbaodd@qq.com>
2022-08-24 13:27:50 +0800

    slice

---
+++ zig/basic/src/slices.zig
@@ -0,0 +1,12 @@
+fn total(values: []const u8) usize {
+    var sum: usize = 0;
+    for (values) |v| sum += v;
+    return sum;
+}
+
+test "slices" {
+    const expect = @import("std").testing.expect;
+    const array = [_]u8{ 1, 2, 3, 4, 5 };
+    const slice = array[0..3];
+    try expect(total(slice) == 6);
+}
