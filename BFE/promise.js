fa94d63c76511a88b5e0e0810cf10dda775d13f9
gongbaodd <gongbaodd@Inspiron.Ubuntu>
2023-03-22 11:46:13 +0800

    promise

--- BFE/promise.js
+++ BFE/promise.js
@@ -13,16 +13,45 @@ class MyPromise {
     this.#value = v;
 
     queueMicrotask(() => {
-      this.#onFulfilled(this.#value);
+      try {
+        const returnValue = this.#onFulfilled(this.#value);
+        if (returnValue instanceof MyPromise) {
+          returnValue.then(this.#then.resolve, this.#then.reject);
+        } else {
+          this.#then.resolve(returnValue);
+        }
+      } catch (e) {
+        this.#then.reject(e);
+      }
     });
   };
   #reject = (e) => {
-    if (this.#state === STATES.PENDING) {
-      this.#state = STATES.REJECTED;
-      this.#value = e;
-    }
+    if (this.#state !== STATES.PENDING) return;
+    this.#state = STATES.REJECTED;
+    this.#value = e;
+
+    queueMicrotask(() => {
+      try {
+        const returnValue = this.#onRejected(this.#value);
+
+        if (returnValue instanceof MyPromise) {
+          returnValue.then(this.#then.resolve, this.#then.reject);
+        } else {
+          this.#then.resolve(returnValue);
+        }
+      } catch (e) {
+        this.#then.reject(e);
+      }
+    });
   };
   #onFulfilled = () => {};
+  #onRejected = (e) => {
+    throw e;
+  };
+  #then = {
+    resolve: () => {},
+    reject: () => {},
+  };
   constructor(executor) {
     try {
       executor(this.#resolve, this.#reject);
@@ -33,12 +62,17 @@ class MyPromise {
 
   then(onFulfilled, onRejected) {
     // your code here
-    this.#onFulfilled = onFulfilled;
-    return new MyPromise((resolve, reject) => {});
+    this.#onFulfilled = onFulfilled ?? this.#onFulfilled;
+    this.#onRejected = onRejected ?? this.#onRejected;
+    return new MyPromise((resolve, reject) => {
+      this.#then.resolve = resolve;
+      this.#then.reject = reject;
+    });
   }
 
   catch(onRejected) {
     // your code here
+    return this.then(undefined, onRejected);
   }
 
   static resolve(value) {
@@ -52,9 +86,26 @@ class MyPromise {
 
   static reject(value) {
     // your code here
-    let myPromise = new MyPromise((resolve, reject) => {
+    let myPromise = new MyPromise((_, reject) => {
       reject(value);
     });
     return myPromise;
   }
 }
+
+/* normal test
+MyPromise.resolve(1).then((v) => {
+  console.log(v); // 1
+});
+*/
+
+//* catch error test
+const mp = new MyPromise((resolve, reject) => {
+  reject("bfe");
+});
+mp.then((data) => {
+  console.log("not called");
+}).catch((error) => {
+  console.log(error); // bfe
+});
+//*/
