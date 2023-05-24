1cdd2cf52d414b3582f40c56e57663a055a18c73
gongbaodd <gongbaodd@Inspiron.Ubuntu>
2023-05-24 15:50:31 +0800

    oneDreive

--- oneDrive/main.ts
+++
@@ -1,7 +0,0 @@
-import "dotenv";
-
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log(Deno.env.get("CLIENT_ID"));
-  console.log(Deno.env.get("TENANT_ID"));
-}
