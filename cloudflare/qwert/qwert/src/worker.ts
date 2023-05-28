1373959828ae8d8c605c011043eb375d3a8c0337
gongbaodd <gongbaodd@Inspiron.Ubuntu>
2023-05-28 10:50:58 +0800

    init project

---
+++ cloudflare/qwert/qwert/src/worker.ts
@@ -0,0 +1,27 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+interface Env {}
+
+// Export a default object containing event handlers
+export default {
+  // The fetch handler is invoked when this worker receives a HTTP(S) request
+  // and should return a Response (optionally wrapped in a Promise)
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    return new Response(
+      `Try making requests to:
+      <ul>
+      <li><code><a href="/redirect?redirectUrl=https://example.com/">/redirect?redirectUrl=https://example.com/</a></code>,</li>
+      <li><code><a href="/proxy?modify&proxyUrl=https://example.com/">/proxy?modify&proxyUrl=https://example.com/</a></code>, or</li>
+      <li><code><a href="/api/todos">/api/todos</a></code></li>`,
+      { headers: { 'Content-Type': 'text/html' } }
+    );
+  },
+};
