5e495d74a50bd372a366d5817234916768cb147c
gongbaodd <gongbaodd@qq.com>
2023-06-21 11:33:15 +0800

    move controller to signal

--- babylon-demo/src/App.tsx
+++ babylon-demo/src/App.tsx
@@ -3,24 +3,24 @@ import "@babylonjs/inspector";
 import { EngineComponent } from "./componets/Babylon"
 import { Loading } from "./scenes/Loading"
 import { Game } from "./scenes/Game"
-import { useSignal } from "@preact/signals-react"
+import { signal } from "@preact/signals-react"
+import { Suspense } from "react"
+
+const isLoading = signal(true)
+
+const onPlay = () => {
+    isLoading.value = false
+}
 
 export const App = () => {
-    const isLoading = useSignal(true)
-    const onPlay = () => {
-        isLoading.value = false
-    }
-
-    console.log("render")
-
     return (
-        <>
+        <Suspense fallback="loading">
             <EngineComponent antialias canvasId="game">
                 {isLoading.value ? <Loading /> : <Game />}
             </EngineComponent>
             <div style={{position: "absolute", top: 0}}>
                 <button onClick={onPlay}>Play</button>
             </div>
-        </>
+        </Suspense>
     )
 }
--- babylon-demo/src/componets/Controller.tsx
+++ babylon-demo/src/componets/Controller.tsx
@@ -1,6 +1,54 @@
 import { ActionManager, ExecuteCodeAction, Scalar } from "@babylonjs/core";
+import { signal } from "@preact/signals-react";
 import { FC, useCallback, useMemo, useState } from "react";
 import { useScene } from "react-babylonjs";
+
+const keyMap = new Map<string, boolean>()
+
+const vertical = signal(0)
+const verticalAxis = signal(0)
+const horizontal = signal(0)
+const horizontalAxis = signal(0)
+
+const dashing = signal(false)
+
+const jumKeyDown = signal(false)
+
+const updateFromKeybaord = () => {
+    if (keyMap.get("ArrowUp")) {
+        vertical.value = Scalar.Lerp(vertical.value, 1, 0.2)
+        verticalAxis.value = 1
+    } else if (keyMap.get("ArrowDown")) {
+        vertical.value = Scalar.Lerp(vertical.value, -1, 0.2)
+        verticalAxis.value = -1
+    } else {
+        vertical.value = 0
+        verticalAxis.value = 0
+    }
+
+    if (keyMap.get("ArrowLeft")) {
+        horizontal.value = Scalar.Lerp(horizontal.value, -1, 0.2)
+        horizontalAxis.value = -1
+    } else if (keyMap.get("ArrowRight")) {
+        horizontal.value = Scalar.Lerp(horizontal.value, 1, 0.2)
+        horizontalAxis.value = 1
+    } else {
+        horizontal.value = 0
+        horizontalAxis.value = 0
+    }
+
+    if (keyMap.get("Shift")) {
+        dashing.value = true
+    } else {
+        dashing.value = false
+    }
+
+    if (keyMap.get(" ")) {
+        jumKeyDown.value = true
+    } else {
+        jumKeyDown.value = false
+    }
+}
 
 interface Props {
     children: React.ReactNode
@@ -8,9 +56,7 @@ interface Props {
 
 export const Controller: FC<Props> = ({ children }) => {
     const scene = useScene()
-    
-    const keyMap = useMemo(() => new Map<string, boolean>(), [])
-    
+        
     const actionManager = useMemo(() => {
         if (!scene) return
         const am = new ActionManager(scene)
@@ -18,43 +64,12 @@ export const Controller: FC<Props> = ({ children }) => {
             keyMap.set(evt.sourceEvent.key, evt.sourceEvent.type === "keydown")
         });
         const keyUp = new ExecuteCodeAction(ActionManager.OnKeyUpTrigger, (evt) => {
-            keyMap.set(evt.sourceEvent.key, evt.sourceEvent.type === "keydown")
+            keyMap.set(evt.sourceEvent.key, evt.sourceEvent.type === "keyup")
         });
         am.registerAction(keyDown)
         am.registerAction(keyUp)
         return am
     }, [scene])
-
-
-    const [vertical, setVertical] = useState(0)
-    const [_verticalAxis, setVerticalAxis] = useState(0)
-    const [horizontal, setHorizontal] = useState(0)
-    const [_horizontalAxis, setHorizontalAxis] = useState(0)
- 
-    const updateFromKeybaord = useCallback(() => {
-        if (keyMap.get("ArrowUp")) {
-            setVertical(Scalar.Lerp(vertical, 1, 0.2))
-            setVerticalAxis(1)
-        } else if (keyMap.get("ArrowDown")) {
-            setVertical(Scalar.Lerp(vertical, -1, 0.2))
-            setVerticalAxis(-1)
-        } else {
-            setVertical(0)
-            setVerticalAxis(0)
-        }
-
-        if (keyMap.get("ArrowLeft")) {
-            setHorizontal(Scalar.Lerp(horizontal, -1, 0.2))
-            setHorizontalAxis(-1)
-        } else if (keyMap.get("ArrowRight")) {
-            setHorizontal(Scalar.Lerp(horizontal, 1, 0.2))
-            setHorizontalAxis(1)
-        } else {
-            setHorizontal(0)
-            setHorizontalAxis(0)
-        }
-
-    }, [keyMap, vertical, horizontal])
     
     return children;
 }
