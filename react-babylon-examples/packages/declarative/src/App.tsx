c6c68c50acbd14d19875238d375e97a458632922
gongbaodd <gongbaodd@qq.com>
2023-06-28 16:10:40 +0800

    derective

--- react-babylon-examples/packages/declarative/src/App.tsx
+++ react-babylon-examples/packages/declarative/src/App.tsx
@@ -1,54 +1,84 @@
-import { Vector3 } from "@babylonjs/core"
+import { Color3, Vector3 } from "@babylonjs/core"
 import { Engine, Scene } from "react-babylonjs"
 import { RotatingBox } from "./RotatingBox"
-import { ChangeEventHandler, useCallback, useState } from "react"
+import { ChangeEventHandler, useCallback, useMemo, useState } from "react"
 
 function App() {
   const [rpm, setRPM] = useState(10)
   const onChange: ChangeEventHandler<HTMLSelectElement> = useCallback(
-    ({target}) => {
+    ({ target }) => {
       setRPM(Number(target.value))
     },
-  [])
+    [])
 
   return <>
-  <label>
-    Select RPM:
-    <select onChange={onChange} defaultValue={rpm}>
-      {[5, 10, 60].map(v =>
-        <option key={v} value={v}>{v}</option>
-      )}
-    </select>
-  </label>
-  <div style={{ flex: 1, display: "flex" }}>
-    <Engine
-      antialias
-      adaptToDeviceRatio
-      canvasId="declarative-canvas"
-      renderOptions={{
-        whenVisibleOnly: true,
-      }}
-    >
-      <Scene>
-        <freeCamera
-          name="camera1"
-          position={new Vector3(0, 5, -10)}
-          setTarget={[Vector3.Zero()]}
-        />
-        <hemisphericLight
-          name="light1"
-          intensity={0.7}
-          direction={new Vector3(0, 1, 0)}
-        />
-        <ground
-          name="ground"
-          width={6}
-          height={6}
-        />
-        <RotatingBox rpm={rpm} />
-      </Scene>
-    </Engine>
-  </div>
+    <label>
+      Select RPM:
+      <select onChange={onChange} defaultValue={rpm}>
+        {[5, 10, 60].map(v =>
+          <option key={v} value={v}>{v}</option>
+        )}
+      </select>
+    </label>
+    <div style={{ flex: 1, display: "flex" }}>
+      <Engine
+        antialias
+        adaptToDeviceRatio
+        canvasId="declarative-canvas"
+        renderOptions={{
+          whenVisibleOnly: true,
+        }}
+      >
+        <Scene>
+          <freeCamera
+            name="camera1"
+            position={new Vector3(0, 5, -10)}
+            setTarget={[Vector3.Zero()]}
+          />
+          <hemisphericLight
+            name="light1"
+            intensity={0.7}
+            direction={new Vector3(0, 1, 0)}
+          />
+          <ground
+            name="ground"
+            width={6}
+            height={6}
+            receiveShadows
+          />
+          <directionalLight
+            name="dl"
+            intensity={.6}
+            direction={useMemo(() => new Vector3(-5 * Math.PI / 4, -5 * Math.PI / 4, -Math.PI), [])}
+            position={useMemo(() => new Vector3(0, 4, 16), [])}
+          >
+            <shadowGenerator
+              mapSize={1024}
+              useBlurExponentialShadowMap
+              blurKernel={64}
+              shadowCastChildren
+            >
+              <RotatingBox
+                name="box1"
+                rpm={rpm}
+                position={useMemo(() => new Vector3(-2, 1, 0), [])}
+                color={useMemo(() => Color3.Red(), [])}
+                hoveredColor={useMemo(() => Color3.Blue(), [])}
+                rotationAxis="y"
+              />
+              <RotatingBox
+                name="box2"
+                rpm={rpm}
+                position={useMemo(() => new Vector3(2, 1, 0), [])}
+                rotationAxis="x"
+                color={useMemo(() => Color3.Green(), [])}
+                hoveredColor={useMemo(() => Color3.Yellow(), [])}
+              />
+            </shadowGenerator>
+          </directionalLight>
+        </Scene>
+      </Engine>
+    </div>
   </>
 }
 
--- react-babylon-examples/packages/declarative/src/RotatingBox.tsx
+++ react-babylon-examples/packages/declarative/src/RotatingBox.tsx
@@ -1,35 +1,61 @@
-import { Vector3 } from "@babylonjs/core"
-import { useState } from "react"
-import { useBeforeRender, useEngine } from "react-babylonjs"
+import { AbstractMesh, Color3, Mesh, Nullable, Vector3 } from "@babylonjs/core"
+import { useEffect, useMemo, useRef, useState } from "react"
+import { useBeforeRender, useEngine, useHover } from "react-babylonjs"
 
 type RotationProps = {
+  name: string
   rpm: number
+  position: Vector3
+  rotationAxis: 'x' | 'y' | 'z'
+  color: Color3
+  hoveredColor: Color3
 }
 
-function RotatingBox({ rpm }: RotationProps) {
-  const [y, setY] = useState(0)
+function RotatingBox({ rpm, position, rotationAxis, color, hoveredColor, name }: RotationProps) {
+  const [axis, setAxis] = useState(0)
   const engine = useEngine()
+  const rotation = useMemo(() => {
+    const r = new Vector3(0,0,0)
+    r[rotationAxis] = axis
+    return r
+  }, [rotationAxis, axis])
+  const boxRef = useRef<Nullable<Mesh>>(null)
+
+  const [hovered, setHovered] = useState(false)
+
+  useHover(
+    () => setHovered(true),
+    () => setHovered(false),
+    boxRef
+  )
 
   useBeforeRender(() => {
     if (!engine) return
 
     const delta = engine.getDeltaTime() / 1000
 
-    setY(_y => {
-      return _y + (rpm / 60) * Math.PI * 2 * delta
+    setAxis(_axis => {
+      return _axis + (rpm / 60) * Math.PI * 2 * delta
     })
   },
   undefined,
   undefined,
   undefined,
-  [engine, rpm])
+  [engine, rpm, setAxis])
 
   return <box
-    name="box"
+    name={name}
     size={2}
-    position={new Vector3(0, 1, 0)}
-    rotation={new Vector3(0, y, 0)}
-  />
+    position={position}
+    rotation={rotation}
+    ref={boxRef}
+  >
+    <standardMaterial
+      name={name + "material"}
+      diffuseColor={hovered ? hoveredColor : color}
+      specularColor={Color3.Black()}
+    />
+  </box>
 }
 
 export { RotatingBox }
