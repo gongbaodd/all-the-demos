7054027d2b6614e585ab33c8fb4f201069d1361d
gongbaodd <gongbaodd@qq.com>
2023-06-29 14:40:38 +0800

    spring

---
+++ react-babylon-examples/packages/spring/src/index.ts
@@ -0,0 +1,18 @@
+import { Globals } from '@react-spring/core';
+import { createHost } from '@react-spring/animated';
+import { createStringInterpolator } from '@react-spring/shared';
+import { primitives } from './primitives';
+import { applyInitialPropsToInstance } from 'react-babylonjs/dist/plugins';
+import { WithAnimated } from './animated';
+
+// todo: frameLoop can use runRenderLoop
+Globals.assign({
+  createStringInterpolator,
+});
+
+const host = createHost(primitives, {
+  applyAnimatedValues: applyInitialPropsToInstance,
+});
+export const animated = host.animated as WithAnimated;
+
+export * from '@react-spring/core';
