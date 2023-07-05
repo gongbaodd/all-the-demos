d3f065a3a0673c4df8b5ef24db34572731d92f9d
gongbaodd <gongbaodd@MacMini>
2023-07-05 14:50:33 +0800

    ItemRow

--- iDine/iDine/iDine/ItemRow.swift
+++ iDine/iDine/iDine/ItemRow.swift
@@ -10,11 +10,33 @@ import SwiftUI
 struct ItemRow: View {
     let item: MenuItem
 
+    let colors: [String: Color] = [
+        "D": .purple,
+        "G": .black,
+        "N": .red,
+        "S": .blue,
+        "V": .green
+    ]
+
     var body: some View {
         HStack {
             Image(item.thumbnailImage)
-            Text(item.name)
-            Text("$\(item.price)")
+                .clipShape(Circle())
+                .overlay(Circle().stroke(.gray, lineWidth: 2))
+            VStack(alignment: .leading) {
+                Text(item.name)
+                    .font(.headline)
+                Text("$\(item.price)")
+            }
+            ForEach(item.restrictions, id: \.self) {
+                restriction in Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(colors[restriction, default: .black])
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
+            }
         }
     }
 }
