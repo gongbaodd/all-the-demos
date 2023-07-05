25cedc2f5837c3b0c06411cc6e9ccd8c0bd6ebd5
gongbaodd <gongbaodd@MacMini>
2023-07-05 14:04:54 +0800

    iDine

---
+++ iDine/iDine/iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by gongbaodd on 2023/7/5.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
