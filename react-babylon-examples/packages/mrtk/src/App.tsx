9c666b326a4b9e6556390f8026685ed28108d6a2
gongbaodd <gongbaodd@Inspiron.Ubuntu>
2023-07-11 16:03:32 +0800

    buttons

--- react-babylon-examples/packages/mrtk/src/App.tsx
+++ react-babylon-examples/packages/mrtk/src/App.tsx
@@ -2,7 +2,8 @@ import { Engine, Scene } from "react-babylonjs";
 import { useMem } from "../../hooks/src/useMem";
 import { Vector3 } from "@babylonjs/core";
 import { TouchHolographicButton } from "@babylonjs/gui/3D/controls/MRTK3/touchHolographicButton";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
+import { GUI3DManager } from "@babylonjs/gui";
 
 TouchHolographicButton.MRTK_ASSET_BASE_URL = "../assets/";
 
@@ -52,10 +53,23 @@ export function App() {
 
 function UI() {
     const mem = useMem()
+    const [manager, setManager] = useState<GUI3DManager|null>(null)
+
+    useEffect(() => {
+        if (!manager) return
+
+        var touchHoloTextButton = new TouchHolographicButton("TouchHoloTextButton");
+        manager.addControl(touchHoloTextButton);
+        touchHoloTextButton.position = new Vector3(0.05, 1.8, 0);
+        touchHoloTextButton.text = "Text Me";
+        touchHoloTextButton.onPointerDownObservable.add(()=>{
+            alert("I display texts")
+        });
+    }, [manager])
     
 
     return (
-        <gui3DManager>
+        <gui3DManager ref={setManager}>
             <touchHolographicButton
                 name="button"
                 position={mem(new Vector3(0.1, 1.7, 0))}
--- react-babylon-examples/packages/mrtk/src/index.tsx
+++ react-babylon-examples/packages/mrtk/src/index.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { App } from "./App"
+import "@babylonjs/loaders/glTF";
 
 const root = createRoot(document.getElementById("root")!)
 root.render(<StrictMode><App/></StrictMode>)
