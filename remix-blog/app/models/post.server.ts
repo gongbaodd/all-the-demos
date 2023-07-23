3d2dd2a19e9d300690241d3f4348064e0d354f78
gongbaodd <gongbaodd@Inspiron.Ubuntu>
2023-07-23 10:16:10 +0800

    db server

--- remix-blog/app/models/post.server.ts
+++ remix-blog/app/models/post.server.ts
@@ -1,13 +1,5 @@
-type Post = {
-  slug: string;
-  title: string;
-};
+import { db } from "../db.server";
 
 export async function getPosts() {
-  const posts: Post[] = [
-    { slug: "my-first-post", title: "My First Post" },
-    { slug: "my-second-post", title: "My Second Post" },
-  ];
-
-  return posts;
+  return db.post.findMany();
 }
