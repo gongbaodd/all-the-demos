688be95cfaffbfa4648cb81764473616ee73f373
gongbaodd <gongbaodd@Inspiron.Ubuntu>
2023-07-23 11:23:34 +0800

    action

--- remix-blog/app/models/post.server.ts
+++ remix-blog/app/models/post.server.ts
@@ -1,23 +1,19 @@
+import { Prisma } from "@prisma/client";
 import { db } from "../db.server";
 
-type Post = {
-  slug: string;
-  title: string;
-};
-
-const posts: Post[] = [
-  { slug: "my-first-post", title: "My First Post" },
-  { slug: "my-second-post", title: "My Second Post" },
-];
 
 export async function getPosts() {
-  // return posts;
   return db.post.findMany();
 }
 
 export async function getPost(slug: string) {
-  // return posts.find((post) => post.slug === slug);
   return db.post.findUnique({
     where: { slug },
   });
+}
+
+type Post = Prisma.PostCreateInput;
+
+export async function createPost(post: Post) {
+  return db.post.create({ data: post });
 }
