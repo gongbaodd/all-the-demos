062eabf10afb397290aa60ca3e084c2e9723f078
gongbaodd <gongbaodd@Inspiron.Ubuntu>
2023-08-15 14:52:57 +0800

    add lit

--- astro-view-transition/astro.config.mjs
+++ astro-view-transition/astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
 
+import lit from "@astrojs/lit";
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    viewTransitions: true,
+    viewTransitions: true
   },
+  integrations: [lit()]
 });
