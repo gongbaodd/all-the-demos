9af03a3d3054ffe0ac65639af1523be3b3182943
gongbaodd@ProArt <gongbaodd@qq.com>
2024-03-01 08:41:22 +0700

    no go for wearables

--- wearos/flutterwatch/flutter_watch/android/app/src/main/kotlin/com/example/flutter_watch/MainActivity.kt
+++ wearos/flutterwatch/flutter_watch/android/app/src/main/kotlin/com/example/flutter_watch/MainActivity.kt
@@ -1,5 +1,21 @@
 package com.example.flutter_watch
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.MotionEvent
+import com.samsung.wearable_rotary.WearableRotaryPlugin
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        intent.putExtra("backgroundMode", "transparent")
+        super.onCreate(savedInstanceState, persistentState)
+    }
+
+    override fun onGenericMotionEvent(event: MotionEvent?): Boolean {
+        return when {
+            WearableRotaryPlugin.onGenericMotionEvent(event) -> true
+            else -> super.onGenericMotionEvent(event)
+        }
+    }
+}
