41cd5180bb89289d9aae5ae6ba513d0f2724e426
gongbaodd@ProArt <gongbaodd@qq.com>
2024-03-12 08:33:48 +0700

    init project

--- uikit/vite.config.ts
+++ uikit/vite.config.ts
@@ -1,22 +1,16 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(), 
-    basicSsl()
-  ],
+  plugins: [react(), basicSsl()],
   optimizeDeps: {
-    include: ['@react-three/uikit-lucide', '@react-three/uikit'],
+    include: ["@react-three/uikit-lucide", "@react-three/uikit"],
   },
-  base: '/uikit/examples/apfel/',
+  base: "/uikit/examples/apfel/",
   resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, '../../packages/kits/apfel') },
-      { find: '@react-three/uikit', replacement: path.resolve(__dirname, '../../packages/uikit/src/index.ts') },
-    ],
+    alias: [{ find: "@", replacement: path.resolve(__dirname, ".") }],
   },
-})
+});
