357fbaaf40eca7cf4c81988ed98211a0a602cfbc
gong@UbuntuWSLProArt <gongaodd@gmail.com>
2024-05-15 20:06:23 +0700

    add ssl support

--- mediapipe/pose/app/index.ts
+++ mediapipe/pose/app/index.ts
@@ -6,20 +6,22 @@ const output = document.getElementById("output")! as HTMLCanvasElement
 const canvasCtx = output.getContext("2d")!
 const drawingUtils = new DrawingUtils(canvasCtx)
 let webCamRunning = false
+let lastVideoTime = -1
+let poseLandmarker: PoseLandmarker | null = null
 
-init()
+
+if (hasGetUserMedia()) {
+    console.log("button is ready")
+    button.addEventListener("click", enableCam)
+    init()
+} else {
+    console.warn("getUserMedia() is not supported byt your browser")
+}
 
 async function init() {
-    if (hasGetUserMedia()) {
-        button.addEventListener("click", enableCam)
-    } else {
-        console.warn("getUserMedia() is not supported byt your browser")
-    }
-    
-
     const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm")
 
-    const poseLandmarker = await PoseLandmarker.createFromOptions(
+    poseLandmarker = await PoseLandmarker.createFromOptions(
         vision,
         {
             baseOptions: {
@@ -28,57 +30,59 @@ async function init() {
             runningMode: "VIDEO"
         }
     )
-
-    function hasGetUserMedia() {
-        return !!navigator.mediaDevices?.getUserMedia
-    }
-    
-    function enableCam(event: MouseEvent) {
-        if (!poseLandmarker){
-            console.warn("Wating poselandmarker to load")
-            return;
-        }    
-    
-        if (true === webCamRunning) {
-            webCamRunning = false
-            button.innerText = "ENABLE PREDICTIONS"
-        } else {
-            webCamRunning = true
-            button.innerText = "DISABLE PREDICTIONS"
-        }
-
-        navigator.mediaDevices.getUserMedia({video: true}).then(stream => {
-            video.srcObject = stream
-            video.addEventListener('loadeddata', predictWebCam)
-        })
-        
-    }
-
-    let lastVideoTime = -1
-    function predictWebCam() {
-        let startTimeMs = performance.now()
-
-        if (lastVideoTime !== video.currentTime) {
-            lastVideoTime = video.currentTime
-            poseLandmarker.detectForVideo(video, startTimeMs, result => {
-                canvasCtx.save()
-                canvasCtx.clearRect(0, 0, output.width, output.height)
-
-                for(const landmark of result.landmarks) {
-                    drawingUtils.drawLandmarks(landmark, {
-                        radius: data => DrawingUtils.lerp(data.from!.z, -.15, .1, 5, 1)
-                    })
-
-                    drawingUtils.drawConnectors(landmark, PoseLandmarker.POSE_CONNECTIONS)
-                }
-
-                canvasCtx.restore()
-            })
-        }
-        
-        if (true === webCamRunning) {
-            window.requestAnimationFrame(predictWebCam)
-        }
-    }
 }
 
+function hasGetUserMedia() {
+    return !!navigator.mediaDevices?.getUserMedia
+}
+
+function enableCam(event: MouseEvent) {
+    if (!poseLandmarker){
+        console.warn("Wating poselandmarker to load")
+        return;
+    }    
+
+    if (true === webCamRunning) {
+        webCamRunning = false
+        button.innerText = "ENABLE PREDICTIONS"
+    } else {
+        webCamRunning = true
+        button.innerText = "DISABLE PREDICTIONS"
+    }
+
+    navigator.mediaDevices.getUserMedia({video: true}).then(stream => {
+        video.srcObject = stream
+        video.addEventListener('loadeddata', predictWebCam)
+    })
+    
+}
+
+function predictWebCam() {
+    if (null === poseLandmarker) {
+        return;
+    }
+
+    let startTimeMs = performance.now()
+
+    if (lastVideoTime !== video.currentTime) {
+        lastVideoTime = video.currentTime
+        poseLandmarker.detectForVideo(video, startTimeMs, result => {
+            canvasCtx.save()
+            canvasCtx.clearRect(0, 0, output.width, output.height)
+
+            for(const landmark of result.landmarks) {
+                drawingUtils.drawLandmarks(landmark, {
+                    radius: data => DrawingUtils.lerp(data.from!.z, -.15, .1, 5, 1)
+                })
+
+                drawingUtils.drawConnectors(landmark, PoseLandmarker.POSE_CONNECTIONS)
+            }
+
+            canvasCtx.restore()
+        })
+    }
+    
+    if (true === webCamRunning) {
+        window.requestAnimationFrame(predictWebCam)
+    }
+}
