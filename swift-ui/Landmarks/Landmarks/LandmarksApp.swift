3b5043d16eff17fa0c59892f6d8d9414d979b842
gongbaodd@macmin <gongbaodd@gmail.com>
2024-07-19 11:35:45 +0800

    creating and combining views

---
+++ swift-ui/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by gongbaodd on 2024/7/19.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
