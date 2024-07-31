0c1f626e5ae0d803eeb060df956d910c8e814f66
gongbaodd@macmin <gongbaodd@gmail.com>
2024-07-31 14:52:01 +0800

    add depth to your app

---
+++ swift-ui/TunnelOfCircles/TunnelOfCircles/TunnelOfCirclesApp.swift
@@ -0,0 +1,18 @@
+//
+//  TunnelOfCirclesApp.swift
+//  TunnelOfCircles
+//
+//  Created by gongbaodd on 2024/7/31.
+//
+
+import SwiftUI
+
+@main
+struct TunnelOfCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowResizability(.contentSize)
+    }
+}
