fc927965b04f6af2ef38dbdd0d41da585bf378f5
gong@UbuntuWSLProArt <gongaodd@gmail.com>
2024-11-08 13:59:26 +0200

    add Sentry integration for error tracking

--- hexagon/astro.config.mjs
+++ hexagon/astro.config.mjs
@@ -3,7 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import sentry from '@sentry/astro';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), sentry({
+    dsn: "https://b3c23c3f710c94a7e1c5ad2d4f295ee4@o360857.ingest.us.sentry.io/4508262508658688",
+    sourceMapsUploadOptions: {
+      project: "javascript",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    },
+  }),]
 });
