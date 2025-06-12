824b66b1b76b4a45dc8977d3062d9abb21d531ad
gong@UbuntuWSLProArt <gongaodd@gmail.com>
2025-06-12 17:08:13 +0300

    start bitecs

--- bitecs-project/src/main.ts
+++ bitecs-project/src/main.ts
@@ -1,24 +1,43 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { addComponent, addEntity, createWorld, defineComponent, defineQuery, pipe, Types, type IWorld } from "bitecs";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const Vector3 = {x: Types.f32, y: Types.f32, z: Types.f32};
+const Position = defineComponent(Vector3);
+const Velocity = defineComponent(Vector3);
+const world = createWorld({
+  time: {
+    delta: 0,
+    elapsed: 0,
+    then: performance.now(),
+  }
+});
+type TWorld = typeof world;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const movementQuery = defineQuery([Position, Velocity]);
+
+const movementSystem = (world: TWorld) => {
+  const ents = movementQuery(world);
+  for (const eid of ents) {
+    Position.x[eid] += Velocity.x[eid];
+    Position.y[eid] += Velocity.y[eid];
+    Position.z[eid] += Velocity.z[eid];
+  }
+  return world;
+}
+
+const timeSystem = (world: TWorld) => {
+  const { time: { then } } = world;
+  world.time.delta = performance.now() - then;
+  world.time.elapsed += world.time.delta;
+  world.time.then = performance.now();
+  return world;
+}
+
+const pipeline = pipe(movementSystem, timeSystem);
+
+const eid = addEntity(world);
+addComponent(world, Position, eid);
+addComponent(world, Velocity, eid);
+
+setInterval(() => {
+  pipeline(world);
+}, 1000 / 60);
